2f1ca5fccf9a6cf9a75d44680fe7d3f91a52600f
Asilbek <azizjonogliasilbek@gmail.com>
2025-10-29 18:19:23 +0800

    feat(invoices): add update and delete invoice functionality with form integration

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,8 @@
 
 import {custom, z} from 'zod';
 import postgres from 'postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'});
 
@@ -14,13 +16,10 @@ const FormSchema = z.object({
 })
 
 const CreateInvoice = FormSchema.omit({id: true, date: true});
+const UpdateInvoice = FormSchema.omit({id: true, date: true});
 
 export async function createInvoice(formData: FormData) {
-    const rawFormData = {
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-    }
+  
     const {customerId, amount, status} = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
@@ -34,5 +33,36 @@ export async function createInvoice(formData: FormData) {
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
 
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 }
 
+export async function updateInvoice(id: string, formData: FormData) {
+
+    const {customerId, amount, status} = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}, date = ${date}
+        WHERE id = ${id}
+    `;
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+
+    await sql`
+        DELETE FROM invoices
+        WHERE id = ${id}
+    `;
+    
+    revalidatePath('/dashboard/invoices');
+}
+    
